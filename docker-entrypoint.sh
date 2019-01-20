dd5b5f027674b0ebf35b99a830c97e1e17e7e08a
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2019-01-21 00:38:05 +0900

    Install CPAN modules in script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,9 @@ find /var/lib/mysql -type f | xargs touch
 service mysql start
 service memcached start
 
+if [ -f t/cpanfile ]; then
+    cpm install -g --test --cpantile=t/cpanfile
+fi
+
 exec "$@"
 
